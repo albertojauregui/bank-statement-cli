d9142793db8b3061c6abbe9731db024e0efd67a7
albertojauregui <bastertwo@gmail.com>
2017-08-18 23:15:58 -0600

    Basic parsing function

--- lib/budget.ex
+++ lib/budget.ex
@@ -1,5 +1,12 @@
 defmodule Budget do
+  alias NimbleCSV.RFC4180, as: CSV
+  
   def list_transactions do
-    "listing transactions"
+    File.read!("lib/transactions.csv")
+    |> parse
+  end
+
+  defp parse(string) do
+    CSV.parse_string(string)
   end
 end
