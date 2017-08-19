8c33f80b7ddf33ad89e5feaaebbd56d7f84c60a8
albertojauregui <bastertwo@gmail.com>
2017-08-18 22:56:43 -0600

    Removed default documentation text

--- lib/budget.ex
+++ lib/budget.ex
@@ -1,17 +1,4 @@
 defmodule Budget do
-  @moduledoc """
-  Documentation for Budget.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Budget.hello
-      :world
-
-  """
   def hello do
     :world
   end
